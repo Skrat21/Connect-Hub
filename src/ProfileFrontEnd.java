fc8736399ee16704cdd94e7d51aa4161f1ea0df4
Skrat21 <155151816+Skrat21@users.noreply.github.com>
2024-12-04 21:30:13 +0200

    okay

--- src/ProfileFrontEnd.java
+++ src/ProfileFrontEnd.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
 public class ProfileFrontEnd {
+    private Image profilePic;
+    private Image coverPic;
+    private String bio;
+
+    public Image getProfilePic() {
+        return profilePic;
+    }
+
+    public Image getCoverPic() {
+        return coverPic;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
 }
--- src/ProfileLoader.java
+++ src/ProfileLoader.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ProfileLoader {
+    // calls backend builds profile
+    public static ProfileFrontEnd load(String profileJson)
+    {
+        Gson gson = new Gson();
+        return gson.fromJson(profileJson,ProfileFrontEnd.class);
+    }
 }
