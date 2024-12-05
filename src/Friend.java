55cfbc82af02d4c7ef3aa1ad39890267486ff967
Jozzef123 <jozefyousry@gmail.com>
2024-12-05 16:35:01 +0200

    Friend

---
+++ src/Friend.java
@@ -0,0 +1,16 @@
+public class Friend {
+    private String name;
+    private String  online;
+
+    public Friend(String name, String online) {
+        this.name = name;
+        this.online = online;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return online;
+}}
