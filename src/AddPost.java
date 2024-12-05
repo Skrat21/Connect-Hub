4d5e053e36de3e1d89a415ec870c8910bfc5b0b1
Jozzef123 <jozefyousry@gmail.com>
2024-12-06 01:15:15 +0200

    added a Jframe that allows a user to add a new post

--- src/AddPost.java
+++ src/AddPost.java
@@ -1,4 +1,89 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
-public class AddPost {
+
+public class AddPost extends JFrame {
+    private JButton postButton;
+    private JFormattedTextField formattedTextField1;
+    private JButton addPhotoButton;
+    private JCheckBox isStoryCheckBox;
+    private JPanel panel1;
+    private String photoPath;
+    private ArrayList<String> photoPaths;
+
+
+    public AddPost(User currentUser) {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panel1);
+        setVisible(true);
+        this.photoPaths = new ArrayList<>();
+
+
+        addPhotoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Select Photos");
+                // Disable the "All files" filter
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                // Allow selecting multiple files
+                fileChooser.setMultiSelectionEnabled(true);
+
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "jpeg", "png");
+                // Add the filter to the file chooser
+                fileChooser.addChoosableFileFilter(filter);
+
+                int returnValue = fileChooser.showOpenDialog(null);
+
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File[] selectedFiles = fileChooser.getSelectedFiles();
+                    // Loop through each selected file
+                    for (File file : selectedFiles) {
+                        photoPaths.add(file.getAbsolutePath());
+                    }
+                }
+            }
+        });
+        postButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String postContent = formattedTextField1.getText();
+
+                if (!postContent.isEmpty()) {
+                    String typeOfContent;
+                    if (isStoryCheckBox.isSelected()) {
+                        typeOfContent = "Story";
+                    } else {
+                        typeOfContent = "Post";
+                    }
+
+                    System.out.println("User ID: " + currentUser.getUserId());
+                    System.out.println("Post content: " + postContent);
+                    System.out.println("type of content:"+ typeOfContent);
+                    System.out.println("filePaths:");
+                    for(String filepath:photoPaths)
+                    {
+                        System.out.println(filepath);
+                    }
+                }
+                else {
+                    JOptionPane.showMessageDialog(panel1,"Please don't leave an empty content field","Error",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        User user = new User("example@example.com", "john_doe", new Date());
+        AddPost addPost = new AddPost(user);
+    }
+
 }
--- src/ImageResizer.java
+++ src/ImageResizer.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+
 public class ImageResizer {
     public static BufferedImage resizeImage(String imagePath, int newWidth, int newHeight) throws IOException {
         // Read the original image from the given file path
@@ -14,10 +15,24 @@ public class ImageResizer {
         Image scaledImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
 
         // Create a new BufferedImage from the scaled image
-        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, originalImage.getType());
         Graphics2D g2d = resizedImage.createGraphics();
         g2d.drawImage(scaledImage, 0, 0, null);
         g2d.dispose();
 
         return resizedImage;
-}}
+    }
+    public static BufferedImage loadImage(String imagePath) throws IOException {
+        // Read the original image from the given file path
+        File inputFile = new File(imagePath);
+        BufferedImage originalImage = ImageIO.read(inputFile);
+
+        // Check if the image was loaded successfully
+        if (originalImage == null) {
+            throw new IOException("Failed to load image from the given path.");
+        }
+
+        // Return the original image without resizing
+        return originalImage;
+    }
+   }
