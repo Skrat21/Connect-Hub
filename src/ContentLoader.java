1f103722b3df6cfa817187f75a72a5a84772f679
Skrat21 <155151816+Skrat21@users.noreply.github.com>
2024-12-06 17:47:36 +0200

    added requested changes

--- src/ContentLoader.java
+++ src/ContentLoader.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 
 public class ContentLoader extends JPanel {
@@ -9,13 +8,23 @@ public class ContentLoader extends JPanel {
     private JTextArea contentTextArea;
     private JLabel profilePictureLabel;
     private JLabel timeLabel;
+    private JLabel typeOfPostLabel;
 
     public ContentLoader(Content content) throws IOException {
         BackEnd backEnd = BackEnd.getInstance();
         contentTextArea.setText(content.getContent());
+        timeLabel.setText(content.getTimestamp().toString());
+        String typeOfPost = "null";
+        if (content instanceof Post) {
+            typeOfPost = "Post";
+        }
+        if (content instanceof Story) {
+            typeOfPost = "Story";
+        }
+        typeOfPostLabel.setText(typeOfPost);
         User author = backEnd.getUser(content.getAuthorId());
         usernameLabel.setText(author.getUsername());
-        Icon profilePicture = (Icon) ImageResizer.resizeImage(author.getProfile().getProfilePhoto(),50,50);
+        Icon profilePicture = (Icon) ImageResizer.resizeImage(author.getProfile().getProfilePhoto(), 50, 50);
         profilePictureLabel.setIcon(profilePicture);
 //        for(String imagePaths: content.getImagePath())
 //        {
