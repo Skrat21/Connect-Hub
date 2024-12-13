25b32480c11b660cf59a111af4d0a03e40fbceb9
Abdelrhman-Ihab <abdelrhman.ihab1@gmail.com>
2024-12-13 20:56:14 +0200

    added classes for handling and storing groups

---
+++ src/Group.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Group {
+    private String groupId;
+    private String creatorId;
+    private ArrayList<String> adminIds;
+    private ArrayList<String> memberIds;
+    private ArrayList<String> requestIds;
+    private String groupName;
+    private String groupBio;
+    private String groupImagePath;
+
+    public Group(String creatorId,String groupName, String groupBio, String groupImagePath) {
+        this.creatorId = creatorId;
+        this.adminIds = new ArrayList<>();
+        this.memberIds = new ArrayList<>();
+        this.requestIds = new ArrayList<>();
+        this.groupName = groupName;
+        this.groupBio = groupBio;
+        this.groupImagePath = groupImagePath;
+        this.groupId = createGroupId();
+    }
+
+    private String createGroupId(){
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        int length = 9;
+        StringBuilder randomString = new StringBuilder(length);
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            randomString.append(characters.charAt(index));
+        }
+        return randomString.toString();
+    }
+
+    public void addMember(String memberId){
+        memberIds.add(memberId);
+    }
+
+    public void removeMember(String memberId){
+        memberIds.remove(memberId);
+    }
+
+    public void addAdmin(String adminId){
+        adminIds.add(adminId);
+    }
+
+    public void removeAdmin(String adminId){
+        adminIds.remove(adminId);
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public ArrayList<String> getAdminIds() {
+        return adminIds;
+    }
+
+    public ArrayList<String> getMemberIds() {
+        return memberIds;
+    }
+
+    public void addRequest(String requestId){
+        requestIds.add(requestId);
+    }
+
+    public void removeRequest(String requestId){
+        requestIds.remove(requestId);
+    }
+
+    public String getGroupId(){
+        return groupId;
+    }
+}
---
+++ src/GroupDatabase.java
@@ -0,0 +1,59 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GroupDatabase {
+
+    public static void storeGroup(Group group) {
+        ArrayList<Group> groups = readJsonFile();
+        groups.add((Group) group);
+        writeJsonFile(groups);
+    }
+
+
+    public static ArrayList<Group> readJsonFile() {
+        try (Reader reader = new FileReader("Groups.json")) {
+            Type listType = new TypeToken<ArrayList<Content>>() {
+            }.getType();
+            Gson gson = new Gson();
+            ArrayList<Group> group = gson.fromJson(reader, listType);
+            return (group != null) ? group : new ArrayList<>();
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    private static void writeJsonFile(ArrayList<Group> groups) {
+        try (Writer writer = new FileWriter("Groups.json")) {
+            Gson gson = new Gson();
+            gson.toJson(groups, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void deleteGroup(Group group) {
+        ArrayList<Group> groups = readJsonFile();
+        groups.remove(group);
+        writeJsonFile(groups);
+    }
+
+    public static Group getGroup(String id) {
+        ArrayList<Group> groups = readJsonFile();
+        for (Group group : groups) {
+            if (group.getGroupId().equals(id)) {
+                return group;
+            }
+        }
+        return null;
+    }
+}
+
