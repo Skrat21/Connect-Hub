9ac6aea3e5ad16cef0a4742c894b048fcdde86a6
Jozzef123 <jozefyousry@gmail.com>
2024-12-13 20:36:29 +0200

    PrimaryAdmins

---
+++ src/PrimaryAdmins.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PrimaryAdmins {
+    private JList list1;
+    private JButton promoteButton;
+    private JButton demoteButton;
+    private JButton editPostButton;
+    private JButton oButton;
+    private JTextField textField1;
+    private JList list2;
+    private JButton leaveButton;
+    private JButton deletePostButton;
+
+    public PrimaryAdmins() {
+        oButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        });
+        leaveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editPostButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        deletePostButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        promoteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        });
+        demoteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        });
+    }
+}
