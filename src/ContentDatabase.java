04809142656d3ed9f8cec453ca2b810c0e6d9fa0
Skrat21 <155151816+Skrat21@users.noreply.github.com>
2024-12-13 18:40:01 +0200

    fixed small bug

--- src/ContentDatabase.java
+++ src/ContentDatabase.java
@@ -1,48 +1,89 @@
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.lang.reflect.Type;
-import java.sql.Time;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class ContentDatabase {
     private static ContentDatabase contentDatabaseInstance = null;
+
     public void storeContent(Content content) {
-        ArrayList<Content> contents = readJsonFile();
-        contents.add(content);
-        writeJsonFile(contents);
+        if (content instanceof Post) {
+            ArrayList<Post> posts = readJsonFilePosts();
+            posts.add((Post) content);
+            writeJsonFilePosts(posts);
+        }
+        if (content instanceof Story) {
+            ArrayList<Story> stories = readJsonFileStories();
+            stories.add((Story) content);
+            writeJsonFileStories(stories);
+        }
     }
 
-    public Content getContent(String contentId) {
-        ArrayList<Content> content = readJsonFile();
-        for (Content contents : content) {
-            if (contents.getContentId().equals(contentId)) {
-                return contents;
+//    public Content getContent(String contentId) {
+//        ArrayList<Content> contents = readJsonFile();
+//        for (Content content : contents) {
+//            if (content.getContentId().equals(contentId)) {
+//                return content;
+//            }
+//        }
+//        return null;
+//    }
+
+    public Post getPost(String contentId) {
+        ArrayList<Post> posts = readJsonFilePosts();
+        for (Post post : posts) {
+            if (post.getContentId().equals(contentId)) {
+                return post;
             }
         }
         return null;
     }
 
-    public ArrayList<Content> getUserPosts(String userId){
-        ArrayList<Content> content = readJsonFile();
-        ArrayList<Content> userPosts = new ArrayList<>();
-        for (Content contents : content) {
-            if (contents.getAuthorId().equals(userId)) {
-                userPosts.add(contents);
+    public Story getStory(String contentId){
+        ArrayList<Story> stories = readJsonFileStories();
+        for(Story story: stories)
+        {
+            if(story.getContentId().equals(contentId))
+            {
+                return story;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Post> getUserPosts(String userId) {
+        ArrayList<Post> posts = readJsonFilePosts();
+        ArrayList<Post> userPosts = new ArrayList<>();
+        for (Post post : posts) {
+            if (post.getAuthorId().equals(userId)) {
+                userPosts.add(post);
             }
         }
         return userPosts;
     }
+    public ArrayList<Story> getUserStories(String userId)
+    {
+        ArrayList<Story> stories = readJsonFileStories();
+        ArrayList<Story> userStories = new ArrayList<>();
+        for(Story story: stories)
+        {
+            if(story.getAuthorId().equals(userId))
+            {
+                userStories.add(story);
+            }
+        }
+        return userStories;
+    }
 
-    public ArrayList<Content> readJsonFile(){
-        try (Reader reader = new FileReader("Content.json")) {
-            Type listType = new TypeToken<ArrayList<Content>>() {}.getType();
-            Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter()).create();
-            ArrayList<Content> content = gson.fromJson(reader, listType);
-            return (content != null) ? content : new ArrayList<>();
+    public ArrayList<Post> readJsonFilePosts() {
+        try (Reader reader = new FileReader("Posts.json")) {
+            Type listType = new TypeToken<ArrayList<Content>>() {
+            }.getType();
+            Gson gson = new Gson();
+            ArrayList<Post> post = gson.fromJson(reader, listType);
+            return (post != null) ? post : new ArrayList<>();
         } catch (FileNotFoundException e) {
             return new ArrayList<>();
         } catch (IOException e) {
@@ -51,23 +92,47 @@ public class ContentDatabase {
         }
     }
 
-    private void writeJsonFile(ArrayList<Content> content) {
-        try (Writer writer = new FileWriter("Content.json")) {
-            Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter()).create();
-            gson.toJson(content, writer);
+    public ArrayList<Story> readJsonFileStories() {
+        try (Reader reader = new FileReader("Stories.json")) {
+            Type listType = new TypeToken<ArrayList<Content>>() {
+            }.getType();
+            Gson gson = new Gson();
+            ArrayList<Story> story = gson.fromJson(reader, listType);
+            return (story != null) ? story : new ArrayList<>();
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    private void writeJsonFilePosts(ArrayList<Post> posts) {
+        try (Writer writer = new FileWriter("Posts.json")) {
+            Gson gson = new Gson();
+            gson.toJson(posts, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void writeJsonFileStories(ArrayList<Story> stories) {
+        try (Writer writer = new FileWriter("Stories.json")) {
+            Gson gson = new Gson();
+            gson.toJson(stories, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //singleton design pattern
-    public synchronized static ContentDatabase getInstance()
-    {
-        if(contentDatabaseInstance==null)
-        {
+    public synchronized static ContentDatabase getInstance() {
+        if (contentDatabaseInstance == null) {
             contentDatabaseInstance = new ContentDatabase();
         }
         return contentDatabaseInstance;
     }
+
     private ContentDatabase() {
 
     }
